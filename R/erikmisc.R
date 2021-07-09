51a4de9510fecfada9df3221b7ee535be6a86d6d
Erik Erhardt <erike@stat.unm.edu>
2021-07-09 08:56:53 -0600

    version 0.1.2

--- R/erikmisc.R
+++ R/erikmisc.R
@@ -1,4 +1,9 @@
+#' @keywords internal
+"_PACKAGE"
+
 #' erikmisc
+#'
+#' \if{html}{\figure{erikmisc.png}{options: align='right' alt='logo' width='120'}}
 #'
 #' Erik Erhardt's miscellaneous functions for solving complex data analysis workflows.
 #'
