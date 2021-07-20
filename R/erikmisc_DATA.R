8e2f981841d517b2ee746cca651112d1e6a976c7
Erik Erhardt <erike@stat.unm.edu>
2021-07-20 00:34:33 -0600

    dat_mtcars_e data 3

--- R/erikmisc_DATA.R
+++ R/erikmisc_DATA.R
@@ -1,12 +1,21 @@
-# mtcars_e
+# https://kbroman.org/pkg_primer/pages/data.html
+# Including datasets
+
+## dat_mtcars_e
 
 #' datasets::mtcars, factored and labeled
 #'
 #' Using \code{datasets::mtcars}, then applying labeled factors and labeling variables with \code{labelled::var_label} based on \code{?datasets::mtcars}
 #'
+#' @docType data
+#' @usage data(dat_mtcars_e)
+#' @keywords datasets
 #' @source \code{?datasets::mtcars}
 #' @format tibble
-"mtcars_e"
+#' @examples
+#' data(dat_mtcars_e)
+#' str(dat_mtcars_e)
+"dat_mtcars_e"
 
 #' @importFrom tibble tibble
 NULL
