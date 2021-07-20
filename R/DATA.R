88ffe4518c042f68a23407e3569724d966dee915
Erik Erhardt <erike@stat.unm.edu>
2021-07-20 00:10:00 -0600

    mtcars_e data

---
+++ R/DATA.R
@@ -0,0 +1,12 @@
+# mtcars_e
+
+#' datasets::mtcars, factored and labeled
+#'
+#' Using \code{datasets::mtcars}, then applying labeled factors and labeling variables with \code{labelled::var_label} based on \code{?datasets::mtcars}
+#'
+#' @source \code{?datasets::mtcars}
+#' @format tibble
+"mtcars_e"
+
+#' @importFrom tibble tibble
+NULL
---
+++ data-raw/make_mtcars_e.R
@@ -0,0 +1,40 @@
+# Data for testing
+mtcars_e <-
+  datasets::mtcars %>%
+  tibble::as_tibble(
+    rownames = "model"
+  ) %>%
+  dplyr::mutate(
+    cyl = cyl %>% factor(levels = c(4, 6, 8), labels = c("four", "six", "eight"))
+  , vs  = vs  %>% factor(levels = c(0, 1), labels = c("V-shaped", "straight"))
+  , am  = am  %>% factor(levels = c(0, 1), labels = c("automatic", "manual"))
+  )
+
+# Label columns
+dat_labels <-
+  tibble::tribble(
+    ~var    , ~label
+  , "model" , "Model"
+  , "mpg"   , "Miles/(US) gallon"
+  , "cyl"   , "Number of cylinders"
+  , "disp"  , "Displacement (cu.in.)"
+  , "hp"    , "Gross horsepower"
+  , "drat"  , "Rear axle ratio"
+  , "wt"    , "Weight (1000 lbs)"
+  , "qsec"  , "1/4 mile time"
+  , "vs"    , "Engine"                     # (0 = V-shaped, 1 = straight)"
+  , "am"    , "Transmission"               # (0 = automatic, 1 = manual)"
+  , "gear"  , "Number of forward gears"
+  , "carb"  , "Number of carburetors"
+  )
+
+for (i_row in 1:nrow(dat_labels)) {
+  labelled::var_label(mtcars_e[[dat_labels[["var"]][i_row] ]]) <- dat_labels[["label"]][i_row]
+}
+
+str(mtcars_e)
+
+usethis::use_data(
+    mtcars_e
+  , overwrite = TRUE
+  )
