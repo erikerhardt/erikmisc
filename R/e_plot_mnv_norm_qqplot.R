5fc4772ee3505c16ca28ab7ce94b3484e2cf31f6
Erik Erhardt <erike@stat.unm.edu>
2022-10-11 00:30:40 -0600

    e_plot_mvn_norm_qqplot, fix example for using dat_mtcars_e and filter

--- R/e_plot_mnv_norm_qqplot.R
+++ R/e_plot_mnv_norm_qqplot.R
@@ -14,15 +14,15 @@
 #'
 #' @examples
 #' dat_mtcars_e %>%
-#'   dplyr::filter(cyl == 4) %>%
+#'   dplyr::filter(cyl == "four") %>%
 #'   dplyr::select(mpg, hp, wt) %>%
 #'   e_plot_mnv_norm_qqplot(name = "cyl = 4")
 #' dat_mtcars_e %>%
-#'   dplyr::filter(cyl == 6) %>%
+#'   dplyr::filter(cyl == "six") %>%
 #'   dplyr::select(mpg, hp, wt) %>%
 #'   e_plot_mnv_norm_qqplot(name = "cyl = 6")
 #' dat_mtcars_e %>%
-#'   dplyr::filter(cyl == 8) %>%
+#'   dplyr::filter(cyl == "eight") %>%
 #'   dplyr::select(mpg, hp, wt) %>%
 #'   e_plot_mnv_norm_qqplot(name = "cyl = 8")
 e_plot_mnv_norm_qqplot <-
@@ -40,7 +40,7 @@ e_plot_mnv_norm_qqplot <-
   stats::qqplot(
       x    = stats::qchisq(stats::ppoints(n), df = p)
     , y    = d
-    , main = paste("QQ Plot MV Normality:", name)
+    , main = paste0("QQ Plot MV Normality: ", name)
     , ylab = "Mahalanobis D2 distance"
     , xlab = "Chi-squared quantiles"
   )
