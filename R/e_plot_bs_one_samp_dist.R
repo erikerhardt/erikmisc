039a481ceab3093588cc5a619559deb301ce297d
Erik Erhardt <erike@stat.unm.edu>
2022-11-21 16:34:53 -0700

    e_plot_bs_*, add ggplot plots

--- R/e_plot_bs_one_samp_dist.R
+++ R/e_plot_bs_one_samp_dist.R
@@ -3,10 +3,16 @@
 #' A function to compare the bootstrap sampling distribution with
 #'   a normal distribution with mean and SEM estimated from the data
 #'
-#' @param dat a list of values
-#' @param N   number of bootstrap iterations
+#' @param dat             a list of values
+#' @param N               number of bootstrap iterations
+#' @param sw_graphics     use either ggplot or R base graphics
+#' @param sw_ggplot_print if ggplot, print the plot or just return the grob
+#' @param conf_level      0.95 for a 95% bootstrap CI
 #'
-#' @return \code{invisible(NULL)}
+#' @return \code{invisible(NULL) or ggplot grob}
+#' @import dplyr
+#' @import ggplot2
+#' @importFrom cowplot plot_grid
 #' @importFrom graphics hist
 #' @importFrom graphics par
 #' @importFrom graphics points
@@ -17,43 +23,135 @@
 #' @export
 #'
 #' @examples
-#' e_plot_bs_one_samp_dist(dat = runif(6))
+#' e_plot_bs_one_samp_dist(dat = runif(15), sw_graphics = "base")
+#' e_plot_bs_one_samp_dist(dat = runif(15), sw_graphics = "ggplot")
 e_plot_bs_one_samp_dist <-
   function(
     dat
   , N = 1e4
+  , sw_graphics = c("ggplot", "base")[1]
+  , sw_ggplot_print = c(TRUE, FALSE)[1]
+  , conf_level = 0.95
   ) {
+  ## dat = runif(6)
 
-  n <- length(dat);
+  n <- length(dat)
   # resample from data
-  sam <- matrix(sample(dat, size = N * n, replace = TRUE), ncol=N);
+  dat_sam <- matrix(sample(dat, size = N * n, replace = TRUE), ncol=N)
   # draw a histogram of the means
-  sam_mean <- colMeans(sam);
-  # save par() settings
-  old_par <- par(no.readonly = TRUE)
-  # make smaller margins
-  par(mfrow=c(2,1), mar=c(3,2,2,1), oma=c(1,1,1,1))
-  # Histogram overlaid with kernel density curve
-  graphics::hist(dat, freq = FALSE, breaks = 6
-      , main = "Plot of data with smoothed density curve")
-  points(density(dat), type = "l")
-  rug(dat)
+  dat_sam_mean <- colMeans(dat_sam)
 
-  graphics::hist(sam_mean, freq = FALSE, breaks = 25
-      , main = "Bootstrap sampling distribution of the mean"
-      , xlab = paste("Data: n =", n
-                   , ", mean =", signif(mean(dat), digits = 5)
-                   , ", se =", signif(stats::sd(dat)/sqrt(n)), digits = 5))
-  # overlay a density curve for the sample means
-  points(density(sam_mean), type = "l")
-  # overlay a normal distribution, bold and red
-  x <- seq(min(sam_mean), max(sam_mean), length = 1000)
-  points(x, stats::dnorm(x, mean = mean(dat), sd = stats::sd(dat)/sqrt(n))
-       , type = "l", lwd = 2, col = "red")
-  # place a rug of points under the plot
-  rug(sam_mean)
-  # restore par() settings
-  par(old_par)
+  # obs mean
+  obs_mean <- mean(dat, na.rm = TRUE)
+  # obs CI
+  prob_CI <- c(lower = (1 - conf_level) / 2, upper = 1 - (1 - conf_level) / 2)
+  ind_CI <- prob_CI * N
+  ind_CI[1] <- floor(ind_CI[1])
+    if (ind_CI[1] == 0) { ind_CI[1] <- 1 }
+  ind_CI[2] <- ceiling(ind_CI[2])
+  CI_limits <- sort(dat_sam_mean)[ind_CI]
 
-  invisible(NULL)
+  if (sw_graphics == c("ggplot", "base")[2]) {
+    # save par() settings
+    old_par <- par(no.readonly = TRUE)
+    # make smaller margins
+    par(mfrow=c(2,1), mar=c(3,2,2,1), oma=c(1,1,1,1))
+    # Histogram overlaid with kernel density curve
+    graphics::hist(dat, freq = FALSE, breaks = ceiling(log(n, base = 1.2))
+        , main = "Data with smoothed density curve")
+    points(density(dat), type = "l")
+    rug(dat)
+
+    graphics::hist(dat_sam_mean, freq = FALSE, breaks = ceiling(log(N, base = 1.2))
+        , main = "Bootstrap sampling distribution of the mean"
+        , xlab = paste("Data: n =", n
+                     , ", mean =", signif(mean(dat), digits = 3)
+                     , ", se =", signif(stats::sd(dat)/sqrt(n)), digits = 3))
+    # overlay a density curve for the sample means
+    points(density(dat_sam_mean), type = "l")
+    # overlay a normal distribution, bold and red
+    x <- seq(min(dat_sam_mean), max(dat_sam_mean), length = 1000)
+    points(x, stats::dnorm(x, mean = mean(dat), sd = stats::sd(dat)/sqrt(n))
+         , type = "l", lwd = 2, col = "red")
+    # place a rug of points under the plot
+    rug(dat_sam_mean)
+    # restore par() settings
+    par(old_par)
+
+    invisible(NULL)
+  } # base
+
+  if (sw_graphics == c("ggplot", "base")[1]) {
+    dat_all <-
+      dplyr::bind_rows(
+        tibble::tibble(
+          val   = dat
+        , group = "Data"
+        )
+      , tibble::tibble(
+          val   = dat_sam_mean
+        , group = "BS"
+        )
+      ) %>%
+      dplyr::mutate(
+        group = group %>% factor(levels = c("Data", "BS"))
+      )
+
+    p1 <- ggplot(dat_all %>% dplyr::filter(group == "Data"), aes(x = val))
+    p1 <- p1 + theme_bw()
+    p1 <- p1 + geom_histogram(aes(y = ..density..), boundary = 0, bins = ceiling(log(n, base = 1.2)))
+    p1 <- p1 + geom_density(alpha = 0.2, fill = "gray50", colour = "black", adjust = 2)
+    p1 <- p1 + labs(
+                  title = "Data with smoothed density curve"
+                , x     = NULL
+                , caption =
+                    paste0(
+                      "Data: n = ", n
+                    , " ,  mean = ", signif(mean(dat), digits = 5)
+                    , " ,  se = ", signif(stats::sd(dat)/sqrt(n), digits = 5)
+                    )
+                )
+
+    p2 <- ggplot(dat_all %>% dplyr::filter(group == "BS"), aes(x = val))
+    p2 <- p2 + theme_bw()
+    p2 <- p2 + geom_histogram(aes(y = ..density..), boundary = 0, bins = ceiling(log(N, base = 1.2)))
+    p2 <- p2 + geom_density(fill = NA, colour = "black", adjust = 2, size = 2, alpha = 0.5)
+    p2 <- p2 + stat_function(
+                  fun = dnorm
+                , args = list(mean = mean(dat_sam_mean), sd = sd(dat_sam_mean))
+                , col = "red"
+                , size = 2
+                , alpha = 0.5
+                )
+    p2 <- p2 + labs(
+                  title = "Bootstrap sampling distribution of the mean"
+                , x     = NULL
+                , caption =
+                    paste0(
+                      "Black is smoothed density histogram.  Red is normal distribution."
+                    , "\nDiff = ", signif(obs_mean, 4)
+                    , " ,  95% CI: ("
+                    , signif(CI_limits[1], 4)
+                    , ", "
+                    , signif(CI_limits[2], 4)
+                    , ")"
+                    )
+                )
+
+    p_arranged <-
+      cowplot::plot_grid(
+        plotlist = list(p1, p2)
+      , nrow = NULL
+      , ncol = 1
+      , labels = "auto"
+      , rel_heights = c(1, 1.5)
+      )
+
+    if (sw_ggplot_print) {
+      p_arranged %>% print()
+    }
+
+    return(p_arranged)
+  } # ggplot
+
 } # e_plot_bs_one_samp_dist
--- R/e_plot_bs_two_samp_diff_dist.R
+++ R/e_plot_bs_two_samp_diff_dist.R
@@ -4,11 +4,17 @@
 #'   of the difference of means from two samples with
 #'   a normal distribution with mean and SEM estimated from the data
 #'
-#' @param dat1 a list of values from Sample 1
-#' @param dat2 a list of values from Sample 2
-#' @param N    number of bootstrap iterations
+#' @param dat1            a list of values from Sample 1
+#' @param dat2            a list of values from Sample 2
+#' @param N               number of bootstrap iterations
+#' @param sw_graphics     use either ggplot or R base graphics
+#' @param sw_ggplot_print if ggplot, print the plot or just return the grob
+#' @param conf_level      0.95 for a 95% bootstrap CI
 #'
 #' @return \code{invisible(NULL)}
+#' @import dplyr
+#' @import ggplot2
+#' @importFrom cowplot plot_grid
 #' @importFrom graphics hist
 #' @importFrom graphics par
 #' @importFrom graphics points
@@ -19,60 +25,172 @@
 #' @export
 #'
 #' @examples
-#' e_plot_bs_two_samp_diff_dist(dat1 = runif(6), dat2 = runif(6) - 2)
+#' e_plot_bs_two_samp_diff_dist(dat1 = runif(15), dat2 = runif(15) - 2, sw_graphics = "base")
+#' e_plot_bs_two_samp_diff_dist(dat1 = runif(15), dat2 = runif(15) - 2, sw_graphics = "ggplot")
 e_plot_bs_two_samp_diff_dist <-
   function(
     dat1
   , dat2
   , N = 1e4
+  , sw_graphics = c("ggplot", "base")[1]
+  , sw_ggplot_print = c(TRUE, FALSE)[1]
+  , conf_level = 0.95
   ) {
 
-  n1 <- length(dat1);
-  n2 <- length(dat2);
+  n1 <- length(dat1)
+  n2 <- length(dat2)
   # resample from data
-  sam1 <- matrix(sample(dat1, size = N * n1, replace = TRUE), ncol=N);
-  sam2 <- matrix(sample(dat2, size = N * n2, replace = TRUE), ncol=N);
+  dat_sam1 <- matrix(sample(dat1, size = N * n1, replace = TRUE), ncol=N)
+  dat_sam2 <- matrix(sample(dat2, size = N * n2, replace = TRUE), ncol=N)
   # calculate the means and take difference between populations
-  sam1_mean <- colMeans(sam1);
-  sam2_mean <- colMeans(sam2);
-  diff_mean <- sam1_mean - sam2_mean;
-  # save par() settings
-  old_par <- par(no.readonly = TRUE)
-  # make smaller margins
-  par(mfrow=c(3,1), mar=c(3,2,2,1), oma=c(1,1,1,1))
-  # Histogram overlaid with kernel density curve
-  graphics::hist(dat1, freq = FALSE, breaks = 6
-      , main = paste("Sample 1", "\n"
-                    , "n =", n1
-                    , ", mean =", signif(mean(dat1), digits = 5)
-                    , ", sd =", signif(stats::sd(dat1), digits = 5))
-      , xlim = range(c(dat1, dat2)))
-  points(density(dat1), type = "l")
-  rug(dat1)
+  dat_sam1_mean <- colMeans(dat_sam1)
+  dat_sam2_mean <- colMeans(dat_sam2)
+  dat_diff_mean <- dat_sam1_mean - dat_sam2_mean
 
-  graphics::hist(dat2, freq = FALSE, breaks = 6
-      , main = paste("Sample 2", "\n"
-                    , "n =", n2
-                    , ", mean =", signif(mean(dat2), digits = 5)
-                    , ", sd =", signif(stats::sd(dat2), digits = 5))
-      , xlim = range(c(dat1, dat2)))
-  points(density(dat2), type = "l")
-  rug(dat2)
+  # obs mean
+  obs_mean <- mean(dat1, na.rm = TRUE) - mean(dat2, na.rm = TRUE)
+  # obs CI
+  prob_CI <- c(lower = (1 - conf_level) / 2, upper = 1 - (1 - conf_level) / 2)
+  ind_CI <- prob_CI * N
+  ind_CI[1] <- floor(ind_CI[1])
+    if (ind_CI[1] == 0) { ind_CI[1] <- 1 }
+  ind_CI[2] <- ceiling(ind_CI[2])
+  CI_limits <- sort(dat_diff_mean)[ind_CI]
 
-  graphics::hist(diff_mean, freq = FALSE, breaks = 25
-      , main = paste("Bootstrap sampling distribution of the difference in means", "\n"
-                   , "mean =", signif(mean(diff_mean), digits = 5)
-                   , ", se =", signif(stats::sd(diff_mean), digits = 5)))
-  # overlay a density curve for the sample means
-  points(density(diff_mean), type = "l")
-  # overlay a normal distribution, bold and red
-  x <- seq(min(diff_mean), max(diff_mean), length = 1000)
-  points(x, stats::dnorm(x, mean = mean(diff_mean), sd = stats::sd(diff_mean))
-       , type = "l", lwd = 2, col = "red")
-  # place a rug of points under the plot
-  rug(diff_mean)
-  # restore par() settings
-  par(old_par)
+  if (sw_graphics == c("ggplot", "base")[2]) {
+    # save par() settings
+    old_par <- par(no.readonly = TRUE)
+    # make smaller margins
+    par(mfrow=c(3,1), mar=c(3,2,2,1), oma=c(1,1,1,1))
+    # Histogram overlaid with kernel density curve
+    graphics::hist(dat1, freq = FALSE, breaks = ceiling(log(n1, base = 1.2))
+        , main = paste("Data 1 with smoothed density curve", "\n"
+                      , "n =", n1
+                      , ", mean =", signif(mean(dat1), digits = 3)
+                      , ", sd =", signif(stats::sd(dat1), digits = 3))
+        , xlim = range(c(dat1, dat2)))
+    points(density(dat1), type = "l")
+    rug(dat1)
 
-  invisible(NULL)
+    graphics::hist(dat2, freq = FALSE, breaks = ceiling(log(n2, base = 1.2))
+        , main = paste("Data 2 with smoothed density curve", "\n"
+                      , "n =", n2
+                      , ", mean =", signif(mean(dat2), digits = 3)
+                      , ", sd =", signif(stats::sd(dat2), digits = 3))
+        , xlim = range(c(dat1, dat2)))
+    points(density(dat2), type = "l")
+    rug(dat2)
+
+    graphics::hist(dat_diff_mean, freq = FALSE, breaks = ceiling(log(N, base = 1.2))
+        , main = paste("Bootstrap sampling distribution of the difference in means", "\n"
+                     , "mean =", signif(mean(dat_diff_mean), digits = 5)
+                     , ", se =", signif(stats::sd(dat_diff_mean), digits = 5)))
+    # overlay a density curve for the sample means
+    points(density(dat_diff_mean), type = "l")
+    # overlay a normal distribution, bold and red
+    x <- seq(min(dat_diff_mean), max(dat_diff_mean), length = 1000)
+    points(x, stats::dnorm(x, mean = mean(dat_diff_mean), sd = stats::sd(dat_diff_mean))
+         , type = "l", lwd = 2, col = "red")
+    # place a rug of points under the plot
+    rug(dat_diff_mean)
+    # restore par() settings
+    par(old_par)
+
+    invisible(NULL)
+  } # base
+
+  if (sw_graphics == c("ggplot", "base")[1]) {
+    dat_all <-
+      dplyr::bind_rows(
+        tibble::tibble(
+          val   = dat1
+        , group = "Data1"
+        )
+      , tibble::tibble(
+          val   = dat2
+        , group = "Data2"
+        )
+      , tibble::tibble(
+          val   = dat_diff_mean
+        , group = "BS"
+        )
+      ) %>%
+      dplyr::mutate(
+        group = group %>% factor(levels = c("Data1", "Data2", "BS"))
+      )
+
+    p1 <- ggplot(dat_all %>% dplyr::filter(group == "Data1"), aes(x = val))
+    p1 <- p1 + theme_bw()
+    p1 <- p1 + geom_histogram(aes(y = ..density..), boundary = 0, bins = ceiling(log(n, base = 1.2)))
+    p1 <- p1 + geom_density(alpha = 0.2, fill = "gray50", colour = "black", adjust = 2)
+    p1 <- p1 + xlim(min(c(dat1, dat2), na.rm = TRUE), max(c(dat1, dat2), na.rm = TRUE))
+    p1 <- p1 + labs(
+                  title = "Data 1 with smoothed density curve"
+                , x     = NULL
+                , caption =
+                    paste0(
+                      "Data: n = ", n1
+                    , " ,  mean = ", signif(mean(dat1), digits = 3)
+                    , " ,  se = ", signif(stats::sd(dat1)/sqrt(n1), digits = 3)
+                    )
+                )
+
+    p2 <- ggplot(dat_all %>% dplyr::filter(group == "Data2"), aes(x = val))
+    p2 <- p2 + theme_bw()
+    p2 <- p2 + geom_histogram(aes(y = ..density..), boundary = 0, bins = ceiling(log(n, base = 1.2)))
+    p2 <- p2 + geom_density(alpha = 0.2, fill = "gray50", colour = "black", adjust = 2)
+    p2 <- p2 + xlim(min(c(dat1, dat2), na.rm = TRUE), max(c(dat1, dat2), na.rm = TRUE))
+    p2 <- p2 + labs(
+                  title = "Data 2 with smoothed density curve"
+                , x     = NULL
+                , caption =
+                    paste0(
+                      "Data: n = ", n2
+                    , " ,  mean = ", signif(mean(dat2), digits = 3)
+                    , " ,  se = ", signif(stats::sd(dat2)/sqrt(n2), digits = 3)
+                    )
+                )
+
+    p3 <- ggplot(dat_all %>% dplyr::filter(group == "BS"), aes(x = val))
+    p3 <- p3 + theme_bw()
+    p3 <- p3 + geom_histogram(aes(y = ..density..), boundary = 0, bins = ceiling(log(N, base = 1.2)))
+    p3 <- p3 + geom_density(fill = NA, colour = "black", adjust = 2, size = 2, alpha = 0.5)
+    p3 <- p3 + stat_function(
+                  fun = dnorm
+                , args = list(mean = mean(dat_diff_mean), sd = sd(dat_diff_mean))
+                , col = "red"
+                , size = 2
+                , alpha = 0.5
+                )
+    p3 <- p3 + labs(
+                  title = "Bootstrap sampling distribution of the difference in means"
+                , x     = NULL
+                , caption =
+                    paste0(
+                      "Black is smoothed density histogram.  Red is normal distribution."
+                    , "\nDiff = ", signif(obs_mean, 4)
+                    , " ,  95% CI: ("
+                    , signif(CI_limits[1], 4)
+                    , ", "
+                    , signif(CI_limits[2], 4)
+                    , ")"
+                    )
+                )
+
+    p_arranged <-
+      cowplot::plot_grid(
+        plotlist = list(p1, p2, p3)
+      , nrow = NULL
+      , ncol = 1
+      , labels = "auto"
+      , rel_heights = c(1, 1, 1.5)
+      )
+
+    if (sw_ggplot_print) {
+      p_arranged %>% print()
+    }
+
+    return(p_arranged)
+  } # ggplot
+
 } # e_plot_bs_two_samp_diff_dist
