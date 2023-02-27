189aab9598b898d6f92e6d75715f65ebb8b2552e
Erik Erhardt <erike@stat.unm.edu>
2023-02-27 13:29:16 -0700

    e_pilot_density_altitude, update doc with tables

--- R/e_pilot_ALL.R
+++ R/e_pilot_ALL.R
@@ -41,8 +41,9 @@
 #' @param sw_propeller           Select propeller to be "fixed_pitch" or "constant_speed"
 #'
 #' @return out                   named list including \code{density_altitude},
-#'                               \code{dist_takeoff_dens_alt}, and
-#'                               \code{climb_rate_dens_alt}
+#'                               \code{dist_takeoff_dens_alt},
+#'                               \code{climb_rate_dens_alt},
+#'                               and all the input parameters.
 #' @export
 #'
 #' @examples
@@ -68,6 +69,87 @@
 #'   , climb_rate_sea_level   = 900
 #'   , sw_propeller = c("fixed_pitch", "constant_speed")[2]
 #'   )
+#'
+#'
+#' ## Tables
+#' # 1972 Piper Arrow II
+#' dist_takeoff_sea_level <- 770
+#' climb_rate_sea_level   <- 900
+#' sw_propeller           <- c("fixed_pitch", "constant_speed")[2]
+#'
+#' list_out <- list()
+#' i_list <- 0
+#'
+#' for (i_pres_alt in seq(0, 10000, by = 2000)) {
+#'   ## i_pres_alt = 0
+#'   for (i_temp_C in c(0, seq(15, 40, by = 5))) {
+#'     ## i_temp_C = 0
+#'
+#'     i_list <- i_list + 1
+#'
+#'     list_out[[ i_list ]] <-
+#'       e_pilot_density_altitude(
+#'         pres_alt               = i_pres_alt
+#'       , temp_C                 = i_temp_C
+#'       , dist_takeoff_sea_level = dist_takeoff_sea_level
+#'       , climb_rate_sea_level   = climb_rate_sea_level
+#'       , sw_propeller           = sw_propeller
+#'       )
+#'   }
+#' }
+#'
+#' dat_out <-
+#'   list_out %>%
+#'   dplyr::bind_rows()
+#' dat_out %>% print(n = 10)
+#'
+#' # Note: pres_alt = pressure altitude at 29.92 inches Mg
+#'
+#' # density altitude
+#' dat_density_altitude <-
+#'   dat_out %>%
+#'   dplyr::select(
+#'     density_altitude
+#'   , pres_alt
+#'   , temp_C
+#'   ) %>%
+#'   tidyr::pivot_wider(
+#'     id_cols     = temp_C
+#'   , names_from  = pres_alt
+#'   , values_from = density_altitude
+#'   )
+#'
+#' # takeoff roll
+#' dat_takeoff_roll <-
+#'   dat_out %>%
+#'   dplyr::select(
+#'     dist_takeoff_dens_alt
+#'   , pres_alt
+#'   , temp_C
+#'   ) %>%
+#'   tidyr::pivot_wider(
+#'     id_cols     = temp_C
+#'   , names_from  = pres_alt
+#'   , values_from = dist_takeoff_dens_alt
+#'   )
+#'
+#' # climb rate
+#' dat_climbrate <-
+#'   dat_out %>%
+#'   dplyr::select(
+#'     climb_rate_dens_alt
+#'   , pres_alt
+#'   , temp_C
+#'   ) %>%
+#'   tidyr::pivot_wider(
+#'     id_cols     = temp_C
+#'   , names_from  = pres_alt
+#'   , values_from = climb_rate_dens_alt
+#'   )
+#'
+#' dat_density_altitude
+#' dat_takeoff_roll
+#' dat_climbrate
 #'
 e_pilot_density_altitude <-
   function(
@@ -105,9 +187,14 @@ e_pilot_density_altitude <-
 
   out <-
     c(
-      density_altitude      = round(density_altitude)
-    , dist_takeoff_dens_alt = round(dist_takeoff_dens_alt)
-    , climb_rate_dens_alt   = round(climb_rate_dens_alt)
+      density_altitude       = round(density_altitude)
+    , dist_takeoff_dens_alt  = round(dist_takeoff_dens_alt)
+    , climb_rate_dens_alt    = round(climb_rate_dens_alt)
+    , pres_alt               = pres_alt
+    , temp_C                 = temp_C
+    , dist_takeoff_sea_level = dist_takeoff_sea_level
+    , climb_rate_sea_level   = climb_rate_sea_level
+    , sw_propeller           = sw_propeller
     )
 
   return(out)
