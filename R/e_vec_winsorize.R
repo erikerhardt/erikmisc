520a67d4a88c8b8a946dc705c5e042682cb526ca
Erik Erhardt <erike@stat.unm.edu>
2023-07-24 18:32:28 -0600

    e_vec_winsorize, add type=1 default for quantile

--- R/e_vec_winsorize.R
+++ R/e_vec_winsorize.R
@@ -2,6 +2,7 @@
 #'
 #' @param x        numeric list or vector
 #' @param fraction fraction between 0 and 0.5 from both sides to Winsorize
+#' @param type     type from `quantile()`
 #'
 #' @return same numeric vector with extreme values replaced
 #' @export
@@ -15,6 +16,7 @@ e_vec_winsorize <-
   function(
     x
   , fraction = 0.05
+  , type = 1
   ) {
 
   if(length(fraction) != 1 || fraction < 0 || fraction > 0.5) {
@@ -22,7 +24,7 @@ e_vec_winsorize <-
   }
 
   # determine limits
-  lim <- quantile(x, probs=c(fraction, 1 - fraction))
+  lim <- quantile(x, probs=c(fraction, 1 - fraction), type = type)
   # replace more extreme values with limits
   x[ x < lim[1] ] <- lim[1]
   x[ x > lim[2] ] <- lim[2]
