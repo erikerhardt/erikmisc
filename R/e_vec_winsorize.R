a853448d339e306f34dda79e594f48aa654e49fd
Erik Erhardt <erike@stat.unm.edu>
2023-07-24 18:05:24 -0600

    e_vec_winsorize, new function

---
+++ R/e_vec_winsorize.R
@@ -0,0 +1,31 @@
+#' Winsorize a numeric list or vector by replacing extreme values with the less extreme values
+#'
+#' @param x        numeric list or vector
+#' @param fraction fraction between 0 and 0.5 from both sides to Winsorize
+#'
+#' @return same numeric vector with extreme values replaced
+#' @export
+#'
+#' @examples
+#'
+#' x <- 1:100
+#' y <- e_vec_winsorize(x, fraction = 0.25)
+#' plot(x, y)
+e_vec_winsorize <-
+  function(
+    x
+  , fraction = 0.05
+  ) {
+
+  if(length(fraction) != 1 || fraction < 0 || fraction > 0.5) {
+    stop("e_vec_winsorize: fraction, bad value")
+  }
+
+  # determine limits
+  lim <- quantile(x, probs=c(fraction, 1 - fraction))
+  # replace more extreme values with limits
+  x[ x < lim[1] ] <- lim[1]
+  x[ x > lim[2] ] <- lim[2]
+
+  return(x)
+}
