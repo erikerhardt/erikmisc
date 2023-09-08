f3a41d3111e4d78c797dfa56bb9561034d139102
Erik Erhardt <erike@stat.unm.edu>
2023-09-08 17:38:18 -0600

    e_ggplot_theme, in data-raw

---
+++ data-raw/other/e_ggplot_theme.R
@@ -0,0 +1,43 @@
+ggplot2::theme_set(
+  ggplot2::theme_bw(
+    base_family = "Roboto"
+  )
+)
+# ggplot2::theme_set(
+#   ggplot2::theme_void(
+#     base_family = "Roboto"
+#   )
+# )
+#
+# ggplot2::theme_update(
+#     axis.text.x =
+#       ggplot2::element_text(
+#         color = "black"
+#       , face = "bold"
+#       , size = 26
+#       , margin = ggplot2::margin(t = 6)
+#       )
+#   , axis.text.y =
+#       ggplot2::element_text(
+#         color = "black"
+#       #, face = "bold"
+#       , size = 22
+#       , hjust = 1
+#       , margin = ggplot2::margin(r = 6)
+#       , family = "Roboto Mono"
+#       )
+#   , panel.grid.major.y =
+#       ggplot2::element_line(
+#         color = "gray90"
+#       , linewidth = 0.6
+#       )
+#   , axis.line.x =
+#       ggplot2::element_line(
+#         color = "black"
+#       , linewidth = 1
+#       )
+#   , plot.margin =
+#       ggplot2::margin(
+#         rep(20, 4)
+#       )
+# )
