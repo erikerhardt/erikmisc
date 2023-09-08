1efcf3924403a61dae60ff0ca85e3e271813cd28
Erik Erhardt <erike@stat.unm.edu>
2023-09-08 17:37:11 -0600

    e_plot_rf_vimp, new function

--- R/e_plot_rf_vimp.R
+++ R/e_plot_rf_vimp.R
@@ -3,14 +3,22 @@
 #' @param v_imp
 #'
 #' @return p
+#' @importFrom tibble as_tibble
+#' @importFrom RColorBrewer brewer.pal
+#' @import dplyr
 #' @import ggplot2
 #' @export
 #'
 #' @examples
-f_vimp_plot <- function(v_imp) {
+#' # o_class <- randomForestSRC::rfsrc(...)
+#' # e_plot_rf_vimp(o_class$importance)
+e_plot_rf_vimp <- function(v_imp) {
 
   dat_vimp <-
     v_imp %>%
+    tibble::as_tibble(
+      rownames = "Var"
+    ) |>
     dplyr::select(
       Var
     , "all"
@@ -30,7 +38,7 @@ f_vimp_plot <- function(v_imp) {
   #library(ggplot2)
   p <- ggplot(dat_vimp, aes(x = reorder(Var, all), y = all, fill = positive))
   p <- p + theme_bw()
-  p <- p + geom_hline(yintercept = 0, alpha = 1/2, size = 1/4)
+  p <- p + geom_hline(yintercept = 0, alpha = 1/2, linewidth = 1/4)
   p <- p + geom_bar(stat = "identity", colour = "black", width = 1/2, alpha = 3/4)
   p <- p + scale_fill_manual(values = myColors, drop = FALSE)
   p <- p + coord_flip()
@@ -47,4 +55,4 @@ f_vimp_plot <- function(v_imp) {
 
   return(p)
 
-}
+} # e_plot_rf_vimp
