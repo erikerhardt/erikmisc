e690862bc7eae61853f32778ac124593866d117d
Erik Erhardt <erikerhardt@gmail.com>
2025-01-11 16:16:24 -0900

    e_log_shift, add sw_symmetric argument

--- R/e_log_shift.R
+++ R/e_log_shift.R
@@ -10,20 +10,84 @@
 #' @param x         numeric vector
 #' @param min_add   value to add to x before taking log, if not specified and min(x) <= 0, then this is determined automatically
 #' @param base_log  base for log
+#' @param sw_symmetric overrides \code{min_add} and chooses constant to give most symmetric distribution after \code{log(x+c)}
 #'
 #' @return numeric vector with attributes
 #' @export
 #'
 #' @examples
 #'
-#' e_log_shift(x = c(0, 1, 2, 3), min_add = 1)
+#' f_print_example <- function(x, x2 = e_log_shift(x)) {
+#'   print(x)
+#'   print(x2)
+#'   par(mfrow = c(1, 2))
+#'   hist(x)
+#'   hist(x2)
+#'   par(mfrow = c(1, 1))
+#'   invisible(NULL)
+#' }
+#'
+#'
+#' e_log_shift(x = c(0, 1, 2, 3))
 #' e_log_shift(x = c(0, 10, 100, 1000), base = 10)
 #' e_log_shift(x = c(-4, -2, 0, 2, 4))
+#' # Symmetric, positive with right skew
+#' x <- rgamma(100, 1, 1) |> sort()
+#' x_log <- e_log_shift(x, sw_symmetric = TRUE)
+#'   print(x)
+#'   print(x_log)
+#'   par(mfrow = c(1, 3))
+#'   hist(x)
+#'   hist(x_log)
+#'   dat_skew <- tibble::tibble(min_add = seq(0, 10, by = 0.01), skewness = NA)
+#'   for (i_row in seq_len(nrow(dat_skew))) {
+#'     ## i_row = 1
+#'     dat_skew$skewness[i_row] <-
+#'       e_log_shift(x, min_add = dat_skew$min_add[i_row]) |>
+#'       moments::skewness()
+#'   } # i_row
+#'   plot(dat_skew$min_add, dat_skew$skewness, type = "l")
+#'   abline(dat_skew$min_add, dat_skew$skewness, type = "l")
+#'   par(mfrow = c(1, 1))
+#'   print(c(skew_x = moments::skewness(x), skew_x_log = moments::skewness(x_log)))
+#' # Symmetric, positive with right skew, less than 1
+#' x <- rgamma(100, 1, 1) |> sort()
+#' x <- x / max(x + 1)
+#' x_log <- e_log_shift(x, sw_symmetric = TRUE)
+#'   print(x)
+#'   print(x_log)
+#'   par(mfrow = c(1, 2))
+#'   hist(x)
+#'   hist(x_log)
+#'   par(mfrow = c(1, 1))
+#'   print(c(skew_x = moments::skewness(x), skew_x_log = moments::skewness(x_log)))
+#' # Symmetric, negative with left skew
+#' x <- -rgamma(100, 1, 1) |> sort()
+#' x_log <- e_log_shift(x, sw_symmetric = TRUE)
+#'   print(x)
+#'   print(x_log)
+#'   par(mfrow = c(1, 2))
+#'   hist(x)
+#'   hist(x_log)
+#'   par(mfrow = c(1, 1))
+#'   print(c(skew_x = moments::skewness(x), skew_x_log = moments::skewness(x_log)))
+#' # Symmetric, positive with left skew
+#' x <- -rgamma(100, 1, 1) |> sort()
+#' x <- x + min(x)
+#' x_log <- e_log_shift(x, sw_symmetric = TRUE)
+#'   print(x)
+#'   print(x_log)
+#'   par(mfrow = c(1, 2))
+#'   hist(x)
+#'   hist(x_log)
+#'   par(mfrow = c(1, 1))
+#'   print(c(skew_x = moments::skewness(x), skew_x_log = moments::skewness(x_log)))
 e_log_shift <-
   function(
     x
-  , min_add   = NULL
-  , base_log  = 2
+  , min_add       = NULL
+  , base_log      = 2
+  , sw_symmetric  = c(FALSE, TRUE)[1]
   ) {
 
   # keep attributes
@@ -36,6 +100,93 @@ e_log_shift <-
     warning("erikmisc::e_log_shift, all values NA")
     return(x)
   }
+
+  # constant for most symmetric distribution
+  if (sw_symmetric) {
+    f_log_optim <-
+      function(
+        min_add       = 0
+      , f_in_x        = x
+      , f_in_base_log = base_log
+      ) {
+
+      x_new <- x + min_add
+
+      x_log <-
+        log(
+          x_new
+        , base = base_log
+        )
+
+      val_skewness <-
+        moments::skewness(
+          x_log
+        , na.rm = TRUE
+        )
+
+      if (any(x_new < 0)) {
+        return(10 * abs(val_skewness))
+      }
+
+      return(abs(val_skewness))
+    } # f_log_optim
+
+    #out_optim <-
+    #  optim(
+    #    par     = min_add
+    #  , fn      = f_log_optim
+    #  , method  = "Brent"
+    #  , lower   = -min(x, na.rm = TRUE) + 1e-12
+    #  , upper   = +max(x, na.rm = TRUE)
+    #  , f_in_x         = x
+    #  , f_in_base_log  = base_log
+    #  )
+
+    #out_optim <-
+    #  stats::uniroot(
+    #    f       = f_log_optim
+    #  #, par     = min_add
+    #  , interval = c(lower   = -min(x, na.rm = TRUE) + 1e-12, upper   = +max(x, na.rm = TRUE) + 1e1)
+    #  #, lower   = -min(x, na.rm = TRUE) + 1e-12
+    #  #, upper   = +max(x, na.rm = TRUE) + 1e1
+    #  , f_in_x         = x
+    #  , f_in_base_log  = base_log
+    #  )
+
+    out_optim <-
+      stats::optimize(
+        f       = f_log_optim
+      #, par     = min_add
+      , interval =
+          c(
+            lower   = -min(abs(x), na.rm = TRUE) + .Machine$double.eps
+          , upper   = +max(abs(x), na.rm = TRUE) + range(x, na.rm = TRUE) + 1e4
+          )
+      , f_in_x         = x
+      , f_in_base_log  = base_log
+      , tol         = .Machine$double.eps^0.5  # 0.25
+      )
+
+
+    #min_add <- out_optim$par
+    min_add <- out_optim$minimum
+
+    x_log <-
+      log(
+        x + min_add
+      , base = base_log
+      )
+
+    if (abs(moments::skewness(x)) < abs(moments::skewness(x_log))) {
+      message("erikmisc::e_log_shift, sw_symmetric=TRUE, skewness of x less than skewness of log(x)")
+    }
+
+    # keep attributes
+    attributes(x_log) <- x_attr
+    attr(x_log, "e_log_shift") <- c(min_add = min_add, base_log = base_log)
+
+    return(x_log)
+  } # if sw_symmetric
 
   # if specified min_add, then do it
   if(!is.null(min_add)) {
