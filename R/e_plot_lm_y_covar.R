63d698ef64023d85c98b5e90e95ee0b34851533e
Erik Erhardt <erike@stat.unm.edu>
2025-06-24 11:54:16 -0600

    e_plot_lm_y_covar, add facet dimensions, correct y_var_name

--- R/e_plot_lm_y_covar.R
+++ R/e_plot_lm_y_covar.R
@@ -2,6 +2,8 @@
 #'
 #' @param form  lm formula for a model
 #' @param dat   data to use
+#' @param sw_facet_rows   in \code{ggplot::facet_grid}, number of rows
+#' @param sw_facet_cols   in \code{ggplot::facet_grid}, number of columns; \code{NULL} to use as many as necessary
 #'
 #' @return    p ggplot facet scatterplot
 #' @importFrom stringr str_detect
@@ -24,8 +26,10 @@
 #' p |> print()
 e_plot_lm_y_covar <-
   function(
-    form = form
-  , dat  = dat
+    form          = form
+  , dat           = dat
+  , sw_facet_rows = 1
+  , sw_facet_cols = NULL
   ) {
 
   ## plot all covariates vs response
@@ -37,45 +41,61 @@ e_plot_lm_y_covar <-
     terms() |>
     attr("variables") |>
     as.character()
-
   # remove "list" artifact element from as.character() and the random effect
   ind_form_terms <-
     stringr::str_detect(string = form_terms, pattern = "list", negate = TRUE)
-
   form_terms <-
     form_terms[ind_form_terms]
+  ind_form_terms_covar <-
+    stringr::str_detect(string = form_terms, pattern = stringr::fixed(form_terms[1]), negate = TRUE)
 
-  # pull out those covariates into a dataset (along with response, time, and program)
+  y_var_name <-
+    form_terms[1]
+  x_var_names <-
+    form_terms[ind_form_terms_covar]
+
+  # for glm, extract the "y" from "cbind(y, 1-y)"
+  y_var_name_components <-
+    stringr::str_split(
+      string    = y_var_name
+    , pattern   = "\\(|,"
+    ) |>
+    unlist()
+  if (y_var_name_components[1] == "cbind") {
+    y_var_name <-
+      y_var_name_components[2]
+  }
+
 
   dat_plot <-
     dat |>
     dplyr::select(
-      tidyselect::one_of(form_terms[1])
-    #, Time_y
-    , tidyselect::one_of(form_terms)
+      tidyselect::one_of(y_var_name)
+    , tidyselect::one_of(x_var_names)
     ) |>
     # convert factor to numeric for plotting
     e_data_convert_factor_to_numeric()
-
-  ind_form_terms_covar <-
-    stringr::str_detect(string = form_terms, pattern = form_terms[1]       , negate = TRUE)
 
   # reshape long
   dat_plot <-
     dat_plot |>
     tidyr::pivot_longer(
-      cols = form_terms[ind_form_terms_covar]
+      cols = tidyselect::all_of(x_var_names)
     )
 
-  library(ggplot2)
-  p <- ggplot(dat_plot, aes_string(x = "value", y = form_terms[1])) #, colour = "surv_prog.factor", shape = "surv_prog.factor", group = "surv_prog.factor"))
-  p <- p + geom_point(alpha = 1/4, position = position_jitter(w = 0.2, h = 0.2))
+  if (is.null(sw_facet_cols)) {
+    sw_facet_cols <- length(form_terms)
+  }
+
+  #library(ggplot2)
+  p <- ggplot(dat_plot, aes(x = .data[[ "value"]], y = .data[[ y_var_name ]]))
+  p <- p + geom_point(alpha = 1/4, position = position_jitter(w = 0.02, h = 0.02))
   p <- p + geom_smooth(method = "gam", formula = y ~ x, size = 1, se = TRUE, span = 1, alpha = 0.25)
   p <- p + theme_bw()
-  p <- p + facet_grid(. ~ name, drop = TRUE, scales = "free_x")
-  p <- p + labs(  title = form_terms[1]  #labelled::var_label(dat[[ form_terms[1] ]]) |> as.character()
+  p <- p + facet_wrap( ~ name, drop = TRUE, scales = "free_x", nrow = sw_facet_rows, ncol = sw_facet_cols)
+  p <- p + labs(  title = y_var_name  #labelled::var_label(dat[[ form_terms[1] ]]) |> as.character()
                 , subtitle = "Scatterplots with covariates"
-                , y = form_terms[1]  #labelled::var_label(dat[[ form_terms[1] ]]) |> as.character()
+                , y = labelled::var_label(dat[[ y_var_name ]]) |> as.character()
                 )
   return(p)
 } # e_plot_lm_y_covar
