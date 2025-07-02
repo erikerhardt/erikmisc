22081cd397ad4178d93a061ae31239b59d80a909
Erik Erhardt <erike@stat.unm.edu>
2025-07-01 22:11:06 -0600

    e_plot_lm_y_covar, implement e_model_extract_var_names

--- R/e_plot_lm_y_covar.R
+++ R/e_plot_lm_y_covar.R
@@ -46,44 +46,17 @@ e_plot_lm_y_covar <-
   ## plot all covariates vs response
 
   # decompose formula into each covariate
-  # identify response and main effect terms from the formula
-  form_terms <-
-    form |>
-    terms() |>
-    attr("variables") |>
-    as.character()
-  # remove "list" artifact element from as.character() and the random effect
-  ind_form_terms <-
-    stringr::str_detect(string = form_terms, pattern = "list", negate = TRUE)
-  form_terms <-
-    form_terms[ind_form_terms]
-  ind_form_terms_covar <-
-    stringr::str_detect(string = form_terms, pattern = stringr::fixed(form_terms[1]), negate = TRUE)
-
-  y_var_name <-
-    form_terms[1]
-  x_var_names <-
-    form_terms[ind_form_terms_covar]
-
-  # for glm, extract the "y" from "cbind(y, 1-y)"
-  y_var_name_components <-
-    stringr::str_split(
-      string    = y_var_name
-    , pattern   = "\\(|,"
-    ) |>
-    unlist()
-  if (y_var_name_components[1] == "cbind") {
-    y_var_name <-
-      y_var_name_components[2]
-  }
+  xy_var_names_list <- e_model_extract_var_names(formula(fit$terms))
+  y_var_name  <- xy_var_names_list$y_var_name
+  x_var_names <- xy_var_names_list$x_var_names
 
   dat_plot <-
-    dat |>
-    dplyr::select(
-      tidyselect::one_of(y_var_name)
-    , tidyselect::one_of(x_var_names)
-    ) |>
-    tidyr::drop_na()
+  dat |>
+  dplyr::select(
+    tidyselect::one_of(y_var_name)
+  , tidyselect::one_of(x_var_names)
+  ) |>
+  tidyr::drop_na()
 
   # simple
   if (sw_version    == c("simple", "detail")[1]) {
